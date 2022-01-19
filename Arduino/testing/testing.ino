ced950ba3c3f0c21268f236f1e05ef0fca234a6a
eatpoopandgrowstrong <CTJH22.19@ICHAT.SP.EDU.SG>
2022-01-19 23:25:27 +0800

    Create testing.ino

---
+++ Arduino/testing/testing.ino
@@ -0,0 +1,40 @@
+/*
+ * TODO: Millis
+ * Setup of Flags for LEDs
+ * Decoder Function
+ * Handshake Readback
+ * 
+ * 
+ */
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  /*
+   *  Standard Handshake Setup
+   *  
+   */
+
+   
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  /*
+   * Run Decoder followed by functions
+   * 
+   * 
+   */
+   
+}
+
+void DecoderFunction(){
+
+  
+}
