72d37cb33ad689005588e7931d859aba68afa158
eatpoopandgrowstrong <CTJH22.19@ICHAT.SP.EDU.SG>
2022-01-20 00:52:45 +0800

    Update testing.ino

--- Arduino/testing/testing.ino
+++ Arduino/testing/testing.ino
@@ -7,20 +7,56 @@
  * 
  */
 
+/* Variable Optimisation Guide:
+ * Flags -> Use boolean, either true or false
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+unsigned long CurrentMillis;
+
+unsigned long 
+boolean LEDFlag = false;
+
+
+
+
+
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(115200); // Use highest officially supported baudrate on Arduino Uno
 
   /*
    *  Standard Handshake Setup
+   *  Serial Communication resets the Arduino Uno, starting the setup sequence
    *  
    */
 
-   
 
+
+
+
+   /*
+    * PinMode Setup
+    * Right now Idea is 5 LEDs
+    * 2 RGB LEDs, might not have enough IO
+    * 5 + 6?
+    * Ideally reserve another pin for DC motor or servo motor
+    * 
+    */
+    //pinMode();
+
+
+
+    
   
 }
 
@@ -31,10 +67,22 @@ void loop() {
    * 
    * 
    */
+   CurrentMillis = millis()
+
+
+
+
+
    
 }
 
 void DecoderFunction(){
+
+  
+}
+
+void LEDTestFunction(){
+
 
   
 }
